1d53d3190be8f98774f8edd17bb97fcbf12b989d
PedroSodreUFES <pedro.malini@edu.ufes.br>
2026-06-21 02:28:06 -0300

    feat: header

--- 1-go-task/src/app/app.component.ts
+++ 1-go-task/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from "./components/main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HeaderComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
